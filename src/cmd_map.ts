9601ac61c98afd7d9804436e5272b5ad38dbc279
krotla <kkornel@o2.pl>
2025-10-17 11:31:52 +0200

    fix: Use PokeAPI object initialized inside State instead of creating it per command

--- src/cmd_map.ts
+++ src/cmd_map.ts
@@ -2,10 +2,9 @@ import { State } from "./state.js";
 import { PokeAPI } from "./pokeapi.js";
 
 export async function commandMap(state: State) {
-    const pokeApi = new PokeAPI();
     const url = state.nextLocationsURL ? state.nextLocationsURL : 
         "https://pokeapi.co/api/v2/location-area/";
-    const locationAreasPack = await pokeApi.fetchLocations(url);
+    const locationAreasPack = await state.pokeAPI.fetchLocations(url);
     const locationAreas = Object.values(locationAreasPack.results);
     for (let area of locationAreas) {
         console.log(area.name);        
@@ -15,10 +14,9 @@ export async function commandMap(state: State) {
 }
 
 export async function commandMapb(state: State) {
-    const pokeApi = new PokeAPI();
     if (state.prevLocationsURL) {
         const url = state.prevLocationsURL
-        const locationAreasPack = await pokeApi.fetchLocations(url);
+        const locationAreasPack = await state.pokeAPI.fetchLocations(url);
         const locationAreas = Object.values(locationAreasPack.results);
         for (let area of locationAreas) {
             console.log(area.name);        
